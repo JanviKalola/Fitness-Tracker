533445f1e4f551b2e9e1ee05a094a370fc036ab5
JanviKalola <153378375+JanviKalola@users.noreply.github.com>
2025-03-15 11:37:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 import streamlit as st
 import numpy as np
 import pandas as pd
-#import os
+import joblib
+import os
 import matplotlib.pyplot as plt
 from sklearn.ensemble import RandomForestRegressor
 
